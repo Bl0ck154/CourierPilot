f0e1f081b692045e3193a5d5297f1d7880da10c3
Bl0ck <36800583+Bl0ck154@users.noreply.github.com>
2026-08-14 07:59:23 +0300

    Bump CourierPilot to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.block154.courierpilot"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     signingConfigs {
