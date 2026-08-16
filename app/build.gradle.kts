56508f2c8afc227507c5d7bb2d6b66e62ddde703
Bl0ck <36800583+Bl0ck154@users.noreply.github.com>
2026-08-16 12:35:39 +0300

    Bump CourierPilot to 0.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.block154.courierpilot"
         minSdk = 30
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.13.0"
+        versionCode = 26
+        versionName = "0.13.1"
     }
 
     signingConfigs {
